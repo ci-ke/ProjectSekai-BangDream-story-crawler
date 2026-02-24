03cae1a4c9caa9307c79b879b0a4bb87529d10f7
ci-ke <14360436+ci-ke@users.noreply.github.com>
2026-02-24 13:00:48 +0800

    add a symbol

--- request_story_util.py
+++ request_story_util.py
@@ -72,6 +72,7 @@ def valid_filename(filename: str) -> str:
         .replace('*', '＊')
         .replace(':', '：')
         .replace('/', '／')
+        .replace('\\', '＼')
         .replace('?', '？')
         .replace('"', "''")
         .replace('\n', ' ')
